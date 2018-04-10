1d8726f1155fe956f6a84987d0d881a1ff3ddb8f
jmorgan20 <37450743+jmorgan20@users.noreply.github.com>
2018-04-09 21:05:28 -0500

    Completed

--- src/P1_OutputLoopResult.java
+++ src/P1_OutputLoopResult.java
@@ -4,21 +4,22 @@
   	Date: 
 */
 
-
-
 public class P1_OutputLoopResult {
+
+	private static String finalResult;
 
 	public static void main(String[] args) {
 		printSumOfSquaresTo5();
 	}
-	
+
 	public static void printSumOfSquaresTo5() {
-		for( int i = 1; i <= 5; i++ ) {
-			int finalResult = 0;
+		int finalResult = 0;
+		for (int i = 1; i <= 5; i++) {
+
 			finalResult = finalResult + i * i;
 		}
-		
-		System.out.println( "Final result is: " + finalResult );
+
+		System.out.println("Final result is: " + finalResult);
 	}
 
 }
--- src/P2_SlashesAndBangs.java
+++ src/P2_SlashesAndBangs.java
@@ -4,19 +4,18 @@
   	Date: 
 */
 
-
-
 /* 2. Create a table below (just using text) that outlines your analysis of the figure. That is, how many
  * of each character on each line? What expression captures the relationship between the line number and
  * how many of the character is printed on each line? Choose your headings based on how you can break down
  * the figure.
   
-Line Number		| HEADING		| HEADING		| HEADING		| HEADING		| HEADING		|	
-
-
-
-
-
+Line Number		| Left slashes 		| Bangs		| Right slashes 		| HEADING		| HEADING		|	
+1                   0                 22          0
+2                    2                18          2
+3                    4                14          4
+4                    6                10          6   
+5                    8                6           8
+6                    10               2           10  
 */
 
 /* 3. Using your analysis from above, in pseudocode describe the algorithm for how to draw the figure
@@ -30,7 +29,20 @@ Line Number		| HEADING		| HEADING		| HEADING		| HEADING		| HEADING		|
 public class P2_SlashesAndBangs {
 
 	public static void main(String[] args) {
+		for (int lineNumber = 0; lineNumber < 6; lineNumber++) {
+			
+			for (int j = 0; j < 2 * lineNumber; j++)
+			System.out.print("\\");
+			
+			for (int j = 0; j < 22 - (4 * lineNumber); j++)
+				System.out.print("!");
+			
+			for (int j = 0; j < 2 * lineNumber; j++)
+			System.out.print("/");
+			
+			System.out.println();
 
+		}
 	}
 
 }
